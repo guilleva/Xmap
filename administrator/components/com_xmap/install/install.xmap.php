71dcdd70e0e9cfb73d2a169f6885578f49fc1354
Guillermo Vargas <guilleva@gmail.com>
2011-04-04 02:02:58 +0000

    Updating install file
    git-svn-id: http://gw.vargas.co.cr/svn/com_xmap/trunk@29 662e6efe-d8c9-de11-9fdf-00d0b7ae443a

--- administrator/components/com_xmap/install/install.xmap.php
+++ administrator/components/com_xmap/install/install.xmap.php
@@ -48,10 +48,10 @@ class com_xmapInstallerScript
      */
     function uninstall($adapter)
     {
+
         echo '<h2>', JText::_("XMAP_UNISTALLING_XMAP_EXTENSIONS"), '</h2>';
         $db = JFactory::getDBO();
-        require_once JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_xmap' . DS . 'helpers' . DS . 'installer.php';
-        $installer = XmapInstaller::getInstance();
+
         $query = "SELECT extension_id from `#__extensions` where type='xmap_ext'";
         $db->setQuery($query);
         $extensions = $db->loadObjectList();
@@ -73,39 +73,35 @@ class com_xmapInstallerScript
         // require_once JPATH_ADMINISTRATOR.DS.'components'.DS.'com_xmap'.DS.'adapters'.DS.'xmap_ext.php';
         require_once JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_xmap' . DS . 'helpers' . DS . 'installer.php';
         $installer = XmapInstaller::getInstance();
+		$results = $installer->discover();
+		$query = 'SELECT extension_id, element, folder FROM #__extensions where type=\'xmap_ext\'';
+		$db->setQuery($query);
+		$installedtmp = $db->loadObjectList('folder');
+		$extensions = Array();
 
-        $path = JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_xmap' . DS . 'extensions';
-        if ($folders = JFolder::folders($path)) {
-            foreach ($folders as $folder) {
-                if (JFile::exists($path . DS . $folder . DS . $folder . '.xml')) {
-                    $folder = $db->getEscaped($folder);
-                    $query = "SELECT extension_id from `#__extensions` where type='xmap_ext' and folder='$folder'";
-                    $db->setQuery($query);
-                    $id = $db->loadResult();
-                    if (!$id) {
-                        if ($installer->discover_install($path . DS . $folder)) {
-                            echo '<p />' . JText::sprintf('XMAP_INSTALLED_EXTENSION_X', $folder);
+		foreach ($results as $e) {
+			if (!isset($installedtmp[$e->folder])) {
+				if ($e->store() && $installer->discover_install($e->extension_id)) {
+					echo '<p />' . JText::sprintf('XMAP_INSTALLED_EXTENSION_X', $e->folder);
 
-                            // Auto-publish the extension if the component is installed
-                            $db->setQuery(
-                                    "SELECT extension_id from `#__extensions` "
-                                   ."where `type`='component' and `name`='$folder'"
-                            );
-                            $id = $db->loadResult();
-                            // if the component is installed, then publish the extension
-                            if ($id) {
-                                $db->setQuery(
-                                        "update `#__extensions` set `enabled`=1 "
-                                       ."WHERE type='xmap_ext' and folder='$folder'"
-                                );
-                                $db->query();
-                            }
-                        } else {
-                            echo '<p />' . JText::sprintf('XMAP_NOT_INSTALLED_EXTENSION_X', $folder);
-                        }
-                    }
-                }
-            }
-        }
+					// Auto-publish the extension if the component is installed
+					$db->setQuery(
+							"SELECT extension_id from `#__extensions` "
+						   ."where `type`='component' and `name`='{$e->folder}'"
+					);
+					$id = $db->loadResult();
+					// if the component is installed, then publish the extension
+					if ($id) {
+						$db->setQuery(
+								"update `#__extensions` set `enabled`=1, `state`=1 "
+							   ."WHERE `extension_id`='{$e->extension_id}'"
+						);
+						$db->query();
+					}
+				}
+			}
+		}
+		return;
+
     }
 }
