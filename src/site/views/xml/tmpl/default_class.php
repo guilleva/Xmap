d81255ae7d6573283999c71ea2b0355c847e568e
Anderson Grudtner Martins <anderson@ostraining.com>
2015-08-12 00:04:21 -0300

    Fixes #88, possible syntax error for non escaped urls

--- src/site/views/xml/tmpl/default_class.php
+++ src/site/views/xml/tmpl/default_class.php
@@ -104,7 +104,7 @@ class OSMapXmlDisplayer extends OSMapDisplayer
             $priority = $this->getProperty('priority', $node->priority, $node->id, 'xml', $node->uid);
 
             echo '<url>' . "\n";
-            echo '<loc>', $link, '</loc>' . "\n";
+            echo "<loc><![CDATA[" . trim($link) . "]]></loc>\n";
             if ($this->canEdit) {
                 if ($this->showTitle) {
                     echo '<title><![CDATA['.$node->name.']]></title>' . "\n";
