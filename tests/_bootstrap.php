072891695d96e8c4d03f1c8ea692b4d6e22d64eb
Anderson Grudtner Martins <anderson@ostraining.com>
2016-05-10 21:12:17 -0300

    Update codeception for tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,5 +1,2 @@
 <?php
 // This is global bootstrap for autoloading
-
-// Load the bootstrap file from Alledia Builder
-require_once '/builder/src/codeception/_bootstrap.php';
--- tests/acceptance/ExtensionInstallerCest.php
+++
@@ -1,9 +0,0 @@
-<?php
-use \AcceptanceTester;
-
-require_once ALLEDIA_BUILDER_PATH . '/src/codeception/acceptance/ExtensionInstallerAbstractCest.php';
-
-class ExtensionInstallerCest extends ExtensionInstallerAbstractCest
-{
-
-}
