09cd69d1ba41e45fd2be0a2aa601b4fefe47c26a
Anderson Grudtner Martins <anderson@ostraining.com>
2016-05-11 16:32:28 -0300

    Fix uninstall SQL

--- src/admin/sql/mysql/uninstall.utf8.sql
+++ src/admin/sql/mysql/uninstall.utf8.sql
@@ -1,3 +1,7 @@
+SET foreign_key_checks = 0;
+
 DROP TABLE IF EXISTS `#__osmap_sitemap`;
 DROP TABLE IF EXISTS `#__osmap_sitemap_menus`;
 DROP TABLE IF EXISTS `#__osmap_sitemap_hidden`;
+
+SET foreign_key_checks = 1;
