027b25b18ace115758fb2af489ba10b15e3f832a
Anderson Grudtner Martins <anderson@ostraining.com>
2016-05-31 14:13:27 -0300

    Restore the joomla content plugin, but renaming

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,5 +8,8 @@
 * Sitemap description moved to HTML menu item
 * Removed metadata fields from sitemap. Use the menu's metadata if needed
 * Sitemap edit form moved to the sitemap form in the admin
+* Plugins doesn't set UID anymore (will be ignored). UID is now a md5 hash of the url
+* Renamed osmap plugin: com_content -> joomlacontent
+* Removed other plugins from the package. Only support the Joomla's native content - moved to the pro package
 
 ## 3.3.0 Free - Last release with J2.5 support
--- src/extensions/plg_osmap_joomlacontent/joomlacontent.php
+++ src/extensions/plg_osmap_joomlacontent/joomlacontent.php
@@ -1,28 +1,13 @@
 <?php
 /**
  * @package   OSMap
- * @copyright 2007-2014 XMap - Joomla! Vargas. All rights reserved.
- * @copyright 2016 Open Source Training, LLC. All rights reserved..
- * @author    Guillermo Vargas <guille@vargas.co.cr>
- * @author    Alledia <support@alledia.com>
- * @license   GNU General Public License version 2 or later; see LICENSE.txt
- *
- * This file is part of OSMap.
- *
- * OSMap is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * any later version.
- *
- * OSMap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with OSMap. If not, see <http://www.gnu.org/licenses/>.
+ * @copyright 2007-2014 XMap - Joomla! Vargas - Guillermo Vargas. All rights reserved.
+ * @copyright 2016 Open Source Training, LLC. All rights reserved.
+ * @contact   www.alledia.com, support@alledia.com
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
  */
-defined('_JEXEC') or die('Restricted access');
+
+defined('_JEXEC') or die();
 
 require_once JPATH_SITE . '/components/com_osmap/helpers/osmap.php';
 require_once JPATH_SITE . '/components/com_content/helpers/route.php';
