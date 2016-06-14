a455a748554cf5049dfe7dcf71de5ae0636ec3d8
Anderson Grudtner Martins <anderson@ostraining.com>
2016-06-14 20:15:09 -0300

    Restores the method to create a default sitemap if none exists

--- src/admin/library/alledia/osmap/Installer/Script.php
+++ src/admin/library/alledia/osmap/Installer/Script.php
@@ -10,6 +10,7 @@
 namespace Alledia\OSMap\Installer;
 
 use Alledia\Installer\AbstractScript;
+use Alledia\OSMap;
 
 defined('_JEXEC') or die();
 
@@ -47,6 +48,8 @@ class Script extends AbstractScript
             $this->checkDeprecatedSitemapColumns();
         }
 
+        $this->createDefaultSitemap();
+
         return true;
     }
 
@@ -69,6 +72,56 @@ class Script extends AbstractScript
 
             // Remove description and metadata from sitemap table
             $this->dropColumnsIfExists('#__osmap_sitemaps', $foundColumns);
+        }
+    }
+
+    /**
+     * Creates a default sitemap if no one is found.
+     *
+     @return void
+     */
+    protected function createDefaultSitemap()
+    {
+        $db = OSMap\Factory::getDbo();
+        // Check if we have any sitemap, otherwise lets create a default one
+        $query = $db->getQuery(true)
+            ->select('COUNT(*)')
+            ->from('#__osmap_sitemaps');
+        $noSitemaps = ((int) $db->setQuery($query)->loadResult()) === 0;
+
+        if ($noSitemaps) {
+            // Get all menus
+            $menus = \ModMenuHelper::getMenus();
+            if (!empty($menus)) {
+                $data = array(
+                    'name'       => 'Default Sitemap',
+                    'is_default' => 1,
+                    'published'  => 1
+                );
+
+                // Create the sitemap
+                \JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_osmap/tables');
+                $row = \JTable::getInstance('Sitemap', 'OSMapTable');
+                $row->save($data);
+
+                $i = 0;
+                foreach ($menus as $menu) {
+                    $query = $db->getQuery(true)
+                        ->select('id')
+                        ->from('#__menu_types')
+                        ->where('menutype = ' . $db->quote($menu->menutype));
+                    $menuTypeId = (int)$db->setQuery($query)->loadResult();
+
+                    $query = $db->getQuery(true)
+                        ->set('sitemap_id = ' . $db->quote($row->id))
+                        ->set('menutype_id = ' . $db->quote($menuTypeId))
+                        ->set('priority = ' . $db->quote('0.5'))
+                        ->set('changefreq = ' . $db->quote('weekly'))
+                        ->set('ordering = ' . $db->quote($i++))
+                        ->insert('#__osmap_sitemap_menus');
+                    $db->setQuery($query)->execute();
+                }
+            }
         }
     }
 }
