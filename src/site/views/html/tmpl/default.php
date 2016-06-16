796474e176771fd15867a74d4f3e6a910bd38498
Anderson Grudtner Martins <anderson@ostraining.com>
2016-06-16 20:23:20 -0300

    Fixes the CSS of HTML sitemap

--- src/site/views/html/tmpl/default.php
+++ src/site/views/html/tmpl/default.php
@@ -19,7 +19,7 @@ if ($this->debug) {
 JHtml::stylesheet('media/com_osmap/css/sitemap-html.css');
 ?>
 
-<div id="osmap">
+<div id="osmap <?php echo $this->debug ? 'osmap-debug' : ''; ?>">
     <?php if ($this->params->get('show_page_heading', 1) && $this->params->get('page_heading') != '') : ?>
         <!-- Heading -->
         <h2><?php echo $this->escape($this->params->get('page_heading')); ?></h2>
