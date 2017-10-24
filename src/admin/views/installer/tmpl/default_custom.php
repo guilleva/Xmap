f4d4e06db9bef0b88125df7a0379d3f8592e62a7
Bill Tomczak <bill@ostraining.com>
2017-10-24 13:42:44 -0700

    Update all file headers

--- src/admin/views/installer/tmpl/default_custom.php
+++ src/admin/views/installer/tmpl/default_custom.php
@@ -2,7 +2,7 @@
 /**
  * @package   OSMap
  * @copyright 2007-2014 XMap - Joomla! Vargas. All rights reserved.
- * @copyright 2016 Open Source Training, LLC. All rights reserved..
+ * @copyright 2016-2017 Open Source Training, LLC. All rights reserved..
  * @author    Guillermo Vargas <guille@vargas.co.cr>
  * @author    Joomlashack <help@joomlashack.com>
  * @license   GNU General Public License version 2 or later; see LICENSE.txt
