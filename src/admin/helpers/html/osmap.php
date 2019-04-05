16af4fdc791a43105ff27c51f1b5abfc60cf2b28
Bill Tomczak <bill@joomlashack.com>
2019-04-05 13:34:47 -0700

    Provide centralized source for lists

--- src/admin/helpers/html/osmap.php
+++ src/admin/helpers/html/osmap.php
@@ -11,45 +11,70 @@ defined('_JEXEC') or die();
 
 JTable::addIncludePath(JPATH_COMPONENT . '/tables');
 
-
-/**
- * @package       OSMap
- * @subpackage    com_osmap
- */
 abstract class JHtmlOSMap
 {
-
     /**
-     * @param    string  $name
-     * @param    string  $value
-     * @param    int     $j
+     * @param string $name
+     * @param string $selected
+     * @param int    $j
+     *
+     * @return string
      */
-    public static function priorities($name, $value = '0.5', $j = 0)
+    public static function priorities($name, $selected = '0.5', $j = 0)
     {
-        // Array of options
-        for ($i = 0.1; $i <= 1; $i += 0.1) {
-            $options[] = JHTML::_('select.option', $i, $i);
+        $options = array();
+        foreach (static::priorityList() as $priority) {
+            $options[] = JHTML::_('select.option', $priority, $priority);
         }
 
-        return JHtml::_('select.genericlist', $options, $name, null, 'value', 'text', $value, $name . $j);
+        return JHtml::_('select.genericlist', $options, $name, null, 'value', 'text', $selected, $name . $j);
     }
 
     /**
-     * @param    string  $name
-     * @param    string  $value
-     * @param    int     $j
+     * @param string $name
+     * @param string $selected
+     * @param int    $j
+     *
+     * @return string
      */
-    public static function changefrequency($name, $value = 'weekly', $j = 0)
+    public static function changefrequency($name, $selected = 'weekly', $j = 0)
     {
-        // Array of options
-        $options[] = JHTML::_('select.option', 'always', JText::_('COM_OSMAP_ALWAYS'));
-        $options[] = JHTML::_('select.option', 'hourly', JText::_('COM_OSMAP_HOURLY'));
-        $options[] = JHTML::_('select.option', 'daily', JText::_('COM_OSMAP_DAILY'));
-        $options[] = JHTML::_('select.option', 'weekly', JText::_('COM_OSMAP_WEEKLY'));
-        $options[] = JHTML::_('select.option', 'monthly', JText::_('COM_OSMAP_MONTHLY'));
-        $options[] = JHTML::_('select.option', 'yearly', JText::_('COM_OSMAP_YEARLY'));
-        $options[] = JHTML::_('select.option', 'never', JText::_('COM_OSMAP_NEVER'));
+        $options = array();
+        foreach (static::frequencyList() as $value => $text) {
+            $options[] = JHTML::_('select.option', $value, $text);
+        }
 
-        return JHtml::_('select.genericlist', $options, $name, null, 'value', 'text', $value, $name . $j);
+        return JHtml::_('select.genericlist', $options, $name, null, 'value', 'text', $selected, $name . $j);
+    }
+
+    /**
+     * @return float[]
+     */
+    public static function priorityList()
+    {
+        $priorities = array();
+        for ($i = 0.1; $i <= 1; $i += 0.1) {
+            $priorities[] = $i;
+        }
+
+        return $priorities;
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function frequencyList()
+    {
+        $frequencies = array(
+            'always'  => JText::_('COM_OSMAP_ALWAYS'),
+            'hourly'  => JText::_('COM_OSMAP_HOURLY'),
+            'daily'   => JText::_('COM_OSMAP_DAILY'),
+            'weekly'  => JText::_('COM_OSMAP_WEEKLY'),
+            'monthly' => JText::_('COM_OSMAP_MONTHLY'),
+            'yearly'  => JText::_('COM_OSMAP_YEARLY'),
+            'never'   => JText::_('COM_OSMAP_NEVER')
+        );
+
+        return $frequencies;
     }
 }
