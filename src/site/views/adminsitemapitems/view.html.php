79f73c4b60df469eedc983aec21555187c7831ae
Bill Tomczak <bill@joomlashack.com>
2019-04-05 12:31:53 -0700

    Prevent direct access to admin view MAP-256

--- src/site/views/adminsitemapitems/view.html.php
+++ src/site/views/adminsitemapitems/view.html.php
@@ -21,12 +21,14 @@ class OSMapViewAdminSitemapItems extends JViewLegacy
      */
     public function display($tpl = null)
     {
+        $this->checkAccess();
+
         $container = OSMap\Factory::getContainer();
 
         try {
             $id = $container->input->getInt('id');
 
-            $this->params = JFactory::getApplication()->getParams();
+            $this->params = OSMap\Factory::getApplication()->getParams();
 
             // Load the sitemap instance
             $this->sitemap     = OSMap\Factory::getSitemap($id, 'standard');
@@ -36,5 +38,31 @@ class OSMapViewAdminSitemapItems extends JViewLegacy
         }
 
         parent::display($tpl);
+    }
+
+    /**
+     * This view should only be available from the backend
+     *
+     * @return void
+     * @throws Exception
+     */
+    protected function checkAccess()
+    {
+        $server  = new JInput(array_change_key_case($_SERVER, CASE_LOWER));
+        $referer = parse_url($server->getString('http_referer'));
+
+        if (!empty($referer['query'])) {
+            parse_str($referer['query'], $query);
+
+            $option = empty($query['option']) ? null : $query['option'];
+            $view   = empty($query['view']) ? null : $query['view'];
+
+            if ($option == 'com_osmap' && $view == 'sitemapitems') {
+                // Good enough
+                return;
+            }
+        }
+
+        throw new Exception(JText::_('JERROR_PAGE_NOT_FOUND'), 404);
     }
 }
