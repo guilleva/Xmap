04cba81101783c07638042beb89e2ce3a57c1688
Dietrich Streifert <dietrich.streifert@googlemail.com>
2019-10-07 15:04:05 +0200

    Avoid code duplication by requiering the existing view.html.php

--- src/site/views/xml/view.xml.php
+++ src/site/views/xml/view.xml.php
@@ -9,61 +9,10 @@
 
 defined('_JEXEC') or die();
 
-use Alledia\OSMap;
 
-jimport('joomla.application.component.view');
-
-
-class OSMapViewXml extends JViewLegacy
-{
-    /**
-     * @var string
-     */
-    protected $type = null;
-
-    /**
-     * @var \Joomla\Registry\Registry
-     */
-    protected $params = null;
-
-    /**
-     * @var \Joomla\Registry\Registry
-     */
-    protected $osmapParams = null;
-
-    /**
-     * @var Alledia\OSMap\Sitemap\Standard
-     */
-    protected $sitemap = null;
-
-    /**
-     * @param null $tpl
-     *
-     * @return void
-     * @throws Exception
-     */
-    public function display($tpl = null)
-    {
-        $container = OSMap\Factory::getContainer();
-
-        // Help to show a clean XML without other content
-        $container->input->set('tmpl', 'component');
-
-        $id = $container->input->getInt('id');
-
-        $this->type        = OSMap\Helper\General::getSitemapTypeFromInput();
-        $this->params      = JFactory::getApplication()->getParams();
-        $this->osmapParams = JComponentHelper::getParams('com_osmap');
-
-        $this->sitemap = OSMap\Factory::getSitemap($id, $this->type);
-
-        if (!$this->sitemap->isPublished) {
-            throw new Exception(JText::_('COM_OSMAP_MSG_SITEMAP_IS_UNPUBLISHED'));
-        }
-
-        parent::display($tpl);
-
-        // Force to show a clean XML without other content or execution plugins
-        jexit();
-    }
-}
+/*
+ * Having a view.xml.php file in views allows to add
+ * a .xml suffix to the alias, if the sitemap is attached to
+ * a menu entry
+ */
+require_once __DIR__ . '/view.html.php';
