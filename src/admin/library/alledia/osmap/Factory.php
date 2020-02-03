5b9b489d618f9bf8810fb0b53ec6f0b785aeefbd
Bill Tomczak <bill@joomlashack.com>
2020-02-03 12:35:43 -0800

    Update file headers

--- src/admin/library/alledia/osmap/Factory.php
+++ src/admin/library/alledia/osmap/Factory.php
@@ -1,10 +1,25 @@
 <?php
 /**
  * @package   OSMap
+ * @contact   www.joomlashack.com, help@joomlashack.com
  * @copyright 2007-2014 XMap - Joomla! Vargas - Guillermo Vargas. All rights reserved.
  * @copyright 2016-2020 Joomlashack.com. All rights reserved.
- * @contact   www.joomlashack.com, help@joomlashack.com
  * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ *
+ * This file is part of OSMap.
+ *
+ * OSMap is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * OSMap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OSMap.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace Alledia\OSMap;
--- src/admin/views/installer/tmpl/default_custom.php
+++ src/admin/views/installer/tmpl/default_custom.php
@@ -1,18 +1,17 @@
 <?php
 /**
  * @package   OSMap
- * @copyright 2007-2014 XMap - Joomla! Vargas. All rights reserved.
- * @copyright 2016-2020 Joomlashack.com. All rights reserved..
- * @author    Guillermo Vargas <guille@vargas.co.cr>
- * @author    Joomlashack <help@joomlashack.com>
- * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ * @contact   www.joomlashack.com, help@joomlashack.com
+ * @copyright 2007-2014 XMap - Joomla! Vargas - Guillermo Vargas. All rights reserved.
+ * @copyright 2016-2020 Joomlashack.com. All rights reserved.
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
  *
  * This file is part of OSMap.
  *
  * OSMap is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
- * any later version.
+ * (at your option) any later version.
  *
  * OSMap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -20,7 +19,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with OSMap. If not, see <http://www.gnu.org/licenses/>.
+ * along with OSMap.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 // No direct access
